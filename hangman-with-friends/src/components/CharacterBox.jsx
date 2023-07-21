535471df8fdbc3aca55b16624cc84f3716971241
Philip Han <46556419+Philipnah@users.noreply.github.com>
2023-07-21 16:43:21 +0200

    Moved hangman project to another repo

--- hangman-with-friends/src/components/CharacterBox.jsx
+++
@@ -1,8 +0,0 @@
-export default function CharacterBox({ character, guessed }) {
-	return (
-		<li className="bg-slate-700 mx-1 my-4 p-2 rounded-md">
-			{guessed && <p>{character}</p>}
-			{!guessed && <p>-</p>}
-		</li>
-	)
-}
